e307660fd45ea76b68570e0c2d3679088f0c8357
Utley <utley2600@gmail.com>
2015-08-25 21:33:47 -0400

    test out node server

---
+++ nodetest.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var port = 3000;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Azure\n');
+}).listen(port);
